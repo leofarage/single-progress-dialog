6cabeb10a9e323ea7fe3883dbb88c3b247ce8c5e
Leonardo Farage Freitas <leonardo@ckl.io>
2016-05-05 18:16:07 -0300

    Pushes project code

---
+++ app/src/main/java/io/ckl/dialogutil/DialogUtils.java
@@ -0,0 +1,40 @@
+package io.ckl.dialogutil;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public class DialogUtils {
+
+	private static ProgressDialog progressDialog;
+
+	private DialogUtils() {
+	}
+
+	public static void showTransparentLoadingDialog(Context context) {
+		showTransparentLoadingDialog(context, false);
+	}
+
+	public static void showTransparentLoadingDialog(Context context, boolean cancelable) {
+		if (progressDialog != null) {
+			progressDialog.setCancelable(cancelable);
+			return;
+		}
+
+		progressDialog = new ProgressDialog(context, R.style.DialogUtil_Theme);
+		progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Small);
+		progressDialog.setCancelable(cancelable);
+		progressDialog.show();
+	}
+
+	public static void hideProgressDialog() {
+		progressDialog = hideDialog(progressDialog);
+	}
+
+	private static ProgressDialog hideDialog(ProgressDialog dialog) {
+		if (dialog != null && dialog.isShowing()) {
+			dialog.dismiss();
+		}
+
+		return null;
+	}
+}
